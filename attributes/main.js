45b56f53d25235c3db16124600a7c9427b089327
Sahitya Singh <166967863+git-Sahitya@users.noreply.github.com>
2024-05-22 20:26:21 +0530

    Add files via upload

---
+++ attributes/main.js
@@ -0,0 +1,17 @@
+var element;
+
+/*element = document.getElementById("header").innerHTML;
+
+console.log(element);*/
+
+document.getElementById("header").innerHTML="<h1>WOW</h1>";
+
+element=document.getElementById("header").innerHTML
+
+console.log(element);
+
+document.getElementById("header").attributes[1].value ="sasa";
+
+element=document.getElementById("header").getAttribute("class");
+
+console.log(element);
