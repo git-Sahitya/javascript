ec44ca0fd318cf2f843e75ca5d12b855f98c3f3a
Sahitya Singh <166967863+git-Sahitya@users.noreply.github.com>
2024-05-22 20:35:02 +0530

    Add files via upload

---
+++ querySelector/main.js
@@ -0,0 +1,9 @@
+var element;
+
+document.querySelector("#header h1").innerHTML="<h1>wow</h1>";
+
+console.log(element);
+
+element= document.querySelectorAll("ul")[1].innerHTML;
+
+console.log(element);
