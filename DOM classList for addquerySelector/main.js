fe6a7da74db2c0362be150218c8410aff1e09333
Sahitya Singh <166967863+git-Sahitya@users.noreply.github.com>
2024-05-22 20:28:55 +0530

    Add files via upload

---
+++ DOM classList for addquerySelector/main.js
@@ -0,0 +1,15 @@
+var element;
+
+document.querySelector("#header").classList.add("xyz,efg");
+
+element=document.querySelector("#header").classList;
+
+console.log(element);
+
+var element;
+
+document.querySelector("#wrapper").classList.remove("abc", "efg");
+
+element= document.querySelector("#wrapper").classList;
+
+console.log(element);
